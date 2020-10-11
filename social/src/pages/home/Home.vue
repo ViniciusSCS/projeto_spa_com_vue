bca72756011f5870aa1d0585235ddf24064d585e
ViniciusSCS <vinicius.eng.comp92@gmail.com>
2020-10-11 13:21:45 -0300

    Estudo Udemy

--- social/src/pages/home/Home.vue
+++ social/src/pages/home/Home.vue
@@ -1,14 +1,14 @@
 <template>
-<div class="">
-  <h2>Social</h2>
+  <div class="">
+    <h2>Social</h2>
 
-</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  data() {
+  data () {
     return {
 
     }
