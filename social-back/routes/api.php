1e3b994ab55483196474bc4df66947fa7322f393
Vinicius Sarmento <vinicius.eng.comp92@gmail.com>
2020-12-23 17:17:16 -0300

    INiciando o Cadastro

--- social-back/routes/api.php
+++ social-back/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use \App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+Route::post('/cadastro', function (Request $request){
+    $data = $request->all();
+    $user = User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => bcrypt($data['password']),
+    ]);
+
+    return $user;
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
