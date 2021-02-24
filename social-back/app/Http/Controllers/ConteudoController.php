1609b496a667874e900851c21e90c1df063d9d6b
VSCS <you@example.com>
2021-02-24 11:46:15 -0300

    Agrupando namespace

--- social-back/app/Http/Controllers/ConteudoController.php
+++ social-back/app/Http/Controllers/ConteudoController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use App\Models\Conteudo;
+use App\Models\{
+    User,
+    Conteudo
+};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
